42f6c612b75171f308774ee0ee69ef0714dda76d
Jason Davies <jason@jasondavies.com>
2010-10-04 00:52:01 +0100

    Minor cleanup

--- WEB-INF/app/actions.js
+++ WEB-INF/app/actions.js
@@ -1,14 +1,14 @@
 var {encode} = require('ringo/base64'),
     {get} = require('ringo/httpclient'),
-    {Response} = require('ringo/webapp/response');
-var {ByteString} = require('binary');
+    {Response} = require('ringo/webapp/response'),
+    {ByteString} = require('binary');
 
 exports.index = function(req) {
     return Response.static(module.resolve('public/about.html'));
 };
 
 exports.proxy = function(req) {
-    var url = req.params.url, raw = get(url);
+    var raw = get(req.params.url);
     return {
         status: 200,
         headers: {'Content-Type': 'text/javascript'},
